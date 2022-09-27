74c043ca2ab18efb82478f122b60dd4250c3fe27
Megha <meghabmugali@gmail.com>
2022-09-27 10:51:36 +0530

    Initial commit -- Dependency injection

---
+++ app/src/main/java/com/example/dependencyinjectionapplication/MyComponent.java
@@ -0,0 +1,18 @@
+package com.example.dependencyinjectionapplication;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * MyComponent -- real estate agent who knows both the buyer and seller
+ */
+
+@Singleton
+@Component(modules = {SharedPrefModule.class}) //SharedPrefModule -- seller
+public interface MyComponent {
+
+    void inject(MainActivity activity); //DependencyInjectionActivity --- buyer
+
+
+}
